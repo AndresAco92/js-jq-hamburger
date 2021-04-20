ec947a20372c289a5215c90ffc3725a6cfeba762
AndresAco92 <andres.antonio.aco92@gmail.com>
2021-04-20 15:31:29 +0200

    Esercizio corretto

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 $(function(){
+  //Esercizio corretto 
 
+  $('.header-right > a').click(function(){
+    $('.hamburger-menu').addClass('active');
+  });
+
+  
+  $('.close').click(function(){
+    $('.hamburger-menu').removeClass('active');
+  });
+
+  /*
   $('.header-right').click(function(){
     $('.hamburger-menu').show(1000);
   });
   
   $('.close').click(function(){
     $('.hamburger-menu').fadeOut(1000);
-  });
+  });*/
 });
